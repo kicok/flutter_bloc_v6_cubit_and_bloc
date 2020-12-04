991201bee2f345c2e9e7d334bc505ac3e3164520
kicok <kicoker@gmail.com>
2020-12-04 17:33:28 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_v6_cubit_and_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_v6_cubit_and_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter bloc v6.x.x',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter_bloc v 6.x.x'),
+      ),
+      body: Center(
+        child: IntrinsicWidth(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Counter Using Cubit'),
+                onPressed: () {},
+              ),
+              RaisedButton(
+                child: Text('Counter Using Bloc'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
